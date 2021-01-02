09babf8056e92019ac376402dc2c1eb0644e5b54
filleben <filleryben@gmail.com>
2021-01-02 18:22:44 +0000

    added emailjs functionality

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+//Send Email function
+function sendMail(contactForm) {
+  emailjs.send("gmail", "Portfolio", {
+      "name": contactForm.name.value,
+      "email": contactForm.email.value,
+      "message": contactForm.message.value,
+    })
+    .then(
+      function (response) {
+        $("#form").slideUp("slow")
+        $('#success').addClass('visible')
+      },
+      function (error) {
+        $("#form").slideUp("slow")
+        $('#failed').addClass('visible')
+      }
+    );
+  return false;
+}
