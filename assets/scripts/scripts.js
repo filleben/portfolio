9f4ad7037fe37234834821f011d04a13363da938
filleben <filleryben@gmail.com>
2021-01-03 10:09:41 +0000

    added js scrolling

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
   $('.sidenav').sidenav();
+  AOS.init();
+  //Scrolling
+  $('.js-hook--about').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top - 100
+    }, 1000);
+  }); 
+
+  $('.js-hook--home').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 1000);
+  });
+
+  $('.js-hook--projects').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top - 100
+    }, 1000);
+  });
+
+  $('.js-hook--contact').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top - 100
+    }, 1000);
+  });
 });
 
 //Send Email function
