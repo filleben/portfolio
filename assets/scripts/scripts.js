753b741302cb0f94d361ae413d39d8077c99fdae
filleben <filleryben@gmail.com>
2021-01-30 15:25:24 +0000

    removed contact form

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,56 +1,48 @@
 $(document).ready(function () {
-  $('.sidenav').sidenav();
-  AOS.init();
+  $('.sidenav').sidenav()
+  AOS.init()
   //Scrolling
   $('.js-hook--about').click(function (e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top - 100
-    }, 1000);
-  }); 
+    $('html, body').animate(
+      {
+        scrollTop: $('#about').offset().top - 100,
+      },
+      1000
+    )
+  })
 
   $('.js-hook--home').click(function (e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    $('html, body').animate({
-        scrollTop: $("#home").offset().top
-    }, 1000);
-  });
+    $('html, body').animate(
+      {
+        scrollTop: $('#home').offset().top,
+      },
+      1000
+    )
+  })
 
   $('.js-hook--projects').click(function (e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    $('html, body').animate({
-        scrollTop: $("#projects").offset().top - 100
-    }, 1000);
-  });
+    $('html, body').animate(
+      {
+        scrollTop: $('#projects').offset().top - 100,
+      },
+      1000
+    )
+  })
 
   $('.js-hook--contact').click(function (e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    $('html, body').animate({
-        scrollTop: $("#contact").offset().top - 100
-    }, 1000);
-  });
-});
-
-//Send Email function
-function sendMail(contactForm) {
-  emailjs.send("gmail", "Portfolio", {
-      "name": contactForm.name.value,
-      "email": contactForm.email.value,
-      "message": contactForm.message.value,
-    })
-    .then(
-      function (response) {
-        $("#form").slideUp("slow")
-        $('#success').addClass('visible')
+    $('html, body').animate(
+      {
+        scrollTop: $('#contact').offset().top - 100,
       },
-      function (error) {
-        $("#form").slideUp("slow")
-        $('#failed').addClass('visible')
-      }
-    );
-  return false;
-}
+      1000
+    )
+  })
+})
